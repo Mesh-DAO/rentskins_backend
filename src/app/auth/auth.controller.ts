6e63a4fb3827dc72f12f4bff0bc32c3f4b21d1e2
XRow <murilloaugustoe081@gmail.com>
2023-06-30 21:16:33 -0300

    feat: docs auth

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,18 +1,30 @@
 import { AuthService } from './auth.service';
 import { Controller, Get, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/auth/steam')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('authenticate')
+  @ApiOperation({ summary: 'Etapa de authenticação' })
+  @ApiResponse({
+    status: 200,
+    description: 'Authenticação com sucesso',
+  })
   @UseGuards(AuthGuard('steam'))
   async authenticate(@Res() res) {
     return await res.redirect('/api/auth/steam/authenticate/callback');
   }
 
   @Get('authenticate/callback')
+  @ApiOperation({ summary: 'Etapa de callback da authenticação' })
+  @ApiResponse({
+    status: 200,
+    description: 'Callback retornado!',
+  })
   @UseGuards(AuthGuard('steam'))
   async authenticateCallback(@Req() req, @Res() res, @Query() params: string) {
     return this.authService.authenticateCallback(req, res, params);
